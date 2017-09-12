52ac01ce37b7227431b3c9ecbac95b9d83e39f03
Mariusz Jurgielewicz <mariusz.jurgielewicz@qiagen.com>
2017-09-11 17:33:09 -0700

    Adding sample csv file

--- src/main/java/csvh2.groovy
+++ src/main/java/csvh2.groovy
@@ -1,14 +1,13 @@
 import groovy.sql.Sql
 import org.h2.Driver
 
-// Create an h2 jdbc in-memory database,
-// calling it what you like, here db1
+// Create an h2 jdbc in-memory database, calling it what you like, here db1
 def db = Sql.newInstance("jdbc:h2:mem:db1", "org.h2.Driver")
 
 // Create a table from your csv file... 
-db.execute("create table gene_names as select * from csvread('gene_names.txt',null, 'charset=UTF-8 fieldSeparator=' || CHAR(9))")
+db.execute("create table gene_names as select * from csvread('CaliforniaPOI.csv',null, 'charset=UTF-8 fieldSeparator=' || CHAR(9))")
 
 // Execute a normal sql query on this table...
-db.eachRow("select * from gene_names") { row -  > 
+db.eachRow("select * from gene_names") { row -> 
     println row
 }
