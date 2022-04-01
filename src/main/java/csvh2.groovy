2a239595a9dc508e76db33f4bc211dc63932dda6
Mariusz S. Jurgielewicz <mordor@mail.com>
2022-04-01 14:08:47 -0700

    20220401

--- src/main/java/csvh2.groovy
+++ src/main/java/csvh2.groovy
@@ -8,6 +8,6 @@ def db = Sql.newInstance("jdbc:h2:mem:db1", "org.h2.Driver")
 db.execute("create table gene_names as select * from csvread('CaliforniaPOI.csv',null, 'charset=UTF-8 fieldSeparator=' || CHAR(9))")
 
 // Execute a normal sql query on this table...
-db.eachRow("select * from gene_names") { row -> 
+db.eachRow("select * from Track") { row -> 
     println row
 }
